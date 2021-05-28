ee94298c12fee5a3ad03bc2bff86d8f7282c6597
Saiful Islam Sojib <saifulislam.sojib56@gmail.com>
2021-05-28 17:20:26 +0600

    add-react-router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,22 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "../SharedComponents/Navbar/Navbar";
+import SignIn from "../SignIn/SignIn";
 import "./App.scss";
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+
+          </Route>
+          <Route path='/login'>
+            <SignIn />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
