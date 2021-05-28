33c0b23e8c02a11a684c840111ac28063a7dfa21
Mir Hussain <mir.hm110@gmail.com>
2021-05-28 18:41:27 +0600

    navbar done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,22 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from "react-router-dom";
 import Contact from "../Contact/Contact";
 import Navbar from "../SharedComponents/Navbar/Navbar";
 import SignIn from "../SignIn/SignIn";
+import Home from "../Home/Home";
 import "./App.scss";
 
 function App() {
   return (
     <div className='App'>
-      <Navbar />
       <Router>
+        <Navbar />
         <Switch>
           <Route exact path='/'>
-
+            <Home />
           </Route>
           <Route path='/login'>
             <SignIn />
