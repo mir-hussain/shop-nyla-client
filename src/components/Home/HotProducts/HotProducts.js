11d75ab1ebedb7670f3e2fbdd0d64a0bf5a7830d
Mir Hussain <mir.hm110@gmail.com>
2021-05-29 12:44:14 +0600

    hot products done

--- src/components/Home/HotProducts/HotProducts.js
+++ src/components/Home/HotProducts/HotProducts.js
@@ -1,29 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./HotProducts.scss";
+import data from "./HotProductData";
 
 const HotProducts = () => {
   return (
     <div className='hot-products-container'>
-      <Link className='hot-link' to='/shop'>
-        <div className='hot-product-card women'>
-          <h1>Women</h1>
-          <p>Spring 2021</p>
-        </div>
-      </Link>
-      <Link className='hot-link' to='/shop'>
-        <div className='hot-product-card men'>
-          <h1>Men</h1>
-          <p>Spring 2021</p>
-        </div>
-      </Link>
-      <Link className='hot-link' to='/shop'>
-        <div className='hot-product-card cap'>
-          <h1>Accessories</h1>
-          <p>Spring 2021</p>
-        </div>
-      </Link>
+      {data.map((data) => (
+        <Card data={data} />
+      ))}
     </div>
+  );
+};
+
+const Card = ({ data }) => {
+  const { category, date, img } = data;
+  return (
+    <Link className='hot-link' to='/shop'>
+      <div className='hot-product-card women'>
+        <img src={img} alt='' />
+        <div className='card-info'>
+          <div className='card-category'>
+            <h1>{category}</h1>
+            <p>{date}</p>
+          </div>
+          <div className='shop-now'>
+            <p className='shop-now-btn'>Shop now</p>
+            <div className='line'></div>
+          </div>
+        </div>
+      </div>
+    </Link>
   );
 };
 
