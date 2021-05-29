02bce0779a9e8df9fbc55b3dc5e862edd3595f28
Saiful Islam Sojib <saifulislam.sojib56@gmail.com>
2021-05-29 12:08:21 +0600

    Add Spinner

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -1,9 +1,17 @@
+import { css } from "@emotion/react";
 import React from 'react';
+import GridLoader from "react-spinners/GridLoader";
+import './Spinner.css';
 
-const Spinner = () => {
+const override = css`
+  display: block;
+  margin: 0 auto;
+`;
+
+const Spinner = ({color, loading, size}) => {
     return (
-        <div>
-            <h1>Loading...</h1>
+        <div className="sweet-loading">
+            <GridLoader color={color||"#717fe0"} loading={loading} css={override} size={size||30} />
         </div>
     );
 };
